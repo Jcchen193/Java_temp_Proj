33160a77f2c13c243f905433cc6cab228c0499e2
Wen Ke Yi (NCS) <wenke.yi@ncs.com.sg>
2021-04-22 10:20:24 +0800

    First Stage Demo
    Signed-off-by: Wen Ke Yi (NCS) <wenke.yi@ncs.com.sg>

---
+++ src/main/java/com/java/example/demo/controller/HelloSpringBootController.java
@@ -0,0 +1,16 @@
+package com.java.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloSpringBootController{
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "<b>Hello Spring Boot!</b>";
+    }
+
+}
